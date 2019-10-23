f275aa2b6202abdf715b20e383cceac8599c62ed
ShaikhNargis146 <39089778+ShaikhNargis146@users.noreply.github.com>
2019-10-23 12:21:03 +0530

    Update BookingController.java

--- src/main/java/com/microservice/springboot/flightList/controller/BookingController.java
+++ src/main/java/com/microservice/springboot/flightList/controller/BookingController.java
@@ -17,6 +17,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("booking")
 public class BookingController {
